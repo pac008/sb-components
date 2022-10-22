0679418ada014821317b644ab1779407b5f9e83f
Miguel Herrera <herreraalcantarmiguelangel@gmail.com>
2022-10-22 23:22:40 +0200

    first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './mylabel.css'
+
+export interface MyLabelProps {
+  /**
+ * Label contents
+ */
+  label: string;
+  /**
+* How large should the Label be?
+*/
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+* dou you want the characters UPPERCASE ?
+*/
+  allCaps?: boolean;
+  /**
+* what color dou you want to  the Label?
+*/
+  color?: 'primary' | 'secondary' | 'tertiary';
+  /**
+* what fontColor "CustomColor" dou you want to  the Label?
+*/
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  allCaps = false,
+  color = 'primary',
+  label = 'No Label',
+  size = 'normal',
+  fontColor
+}: MyLabelProps) => {
+  console.log(fontColor)
+  return (
+    <span 
+      className={`${size} text-${color} label`}
+      style={{color:fontColor}}>
+      {allCaps ? label.toUpperCase() : label}
+      </span>
+  )
+}
